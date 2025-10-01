04d32ec90f9b4b1a06097a6559b383e65e9a4734
{SZSage} <102438457+SZSage@users.noreply.github.com>
2025-09-30 18:36:07 -0700

    fix firebase config

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -4,7 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration
 const firebaseConfig = {
-  //apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
