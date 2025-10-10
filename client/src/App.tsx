5a958a3a8bf47ec42ba04e5b6b55643c2d700007
{SZSage} <102438457+SZSage@users.noreply.github.com>
2025-10-10 11:40:08 -0700

    feat: UI changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { AudioCall } from './components/AudioCall'
+import { Header } from './components/Header'
 
 function App() {
 
   return (
     <>
+      <div className="flex border-1 border-accent h-12 items-center">
+        <Header />
+      </div>
       <AudioCall>
       </AudioCall>
     </>
