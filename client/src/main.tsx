0b27c48ec85d1c20e984ddc5397fe84ee1ccc475
{SZSage} <102438457+SZSage@users.noreply.github.com>
2025-10-10 11:39:52 -0700

    feat: add react router routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useNavigate, BrowserRouter, Route, Routes } from "react-router"
 import './index.css'
 import App from './App.jsx'
+import { CallRoom } from './components/CallRoom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/callRoom/:callId" element={<CallRoom />}>
+        </Route>
+        <Route path="/" element={<App />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
